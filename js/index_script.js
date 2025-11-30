bf47569817b972ea05ce3bba3c3fbabac9159b67
felipeweil <felipeweilmar@gmail.com>
2025-11-30 17:09:32 +0100

    Added JS
    NEW
    - js connected to index.html
    - font face implemented through file instead of API

---
+++ js/index_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // const urlParams = new URLSearchParams(window.location.search); // Get category selected from index
+  // let category = urlParams.get('category');
+  
+  console.log("Online");
+  
+
+
+});
